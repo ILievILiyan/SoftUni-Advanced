e9b51487c2954efc9ed80f61161534ccbb22efae
ILievILiyan <109824839+ILievILiyan@users.noreply.github.com>
2023-01-31 21:00:44 +0200

    Create 4_rectangle_v2.py

---
+++ 04-Functions-Advanced/lab/4_rectangle_v2.py
@@ -0,0 +1,14 @@
+def rectangle(length, width):
+    def rectangle_area():
+        return length * width
+
+    def rectangle_perimeter():
+        return 2 * (width + length)
+
+    if type(length) != int or type(width) != int:
+        return "Enter valid values!"
+
+    return f"Rectangle area: {rectangle_area()}\nRectangle perimeter: {rectangle_perimeter()}"
+
+
+print(rectangle(2, 10))
